a7c381a6b0b1a4523fc758e213df5b0e85b07ce2
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2024-02-12 12:11:43 +0000

    Flask starter code added. Seperation of Controller and Entity implemented.

--- app/main/routes/userRoute.py
+++ app/main/routes/userRoute.py
@@ -1,5 +1,18 @@
 from app.main import bp
 
+import requests
+
+
 @bp.route('/hi')
 def hello_From_controller():
-    return"I am controller"
+    return "I am controller"
+
+
+@bp.route('/getData')
+def getAPIData():
+    API_KEY = 'a6a27a0d825d27f290184340b4de29ce3080eecb'
+    CONTRACT_NAME = 'dublin'
+    data = requests.get(
+        'https://api.jcdecaux.com/vls/v1/stations?contract=' + CONTRACT_NAME + '&apiKey=' + API_KEY + '')
+    print(data.json())
+    return data.json()
