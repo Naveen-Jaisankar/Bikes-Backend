72439e90b20e02d991201d4de2a47b928b58a307
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2024-03-09 17:32:49 +0000

    Stations displayed on map and their corresponding data displayed on mouse hover

--- app/main/routes/userRoute.py
+++ app/main/routes/userRoute.py
@@ -6,7 +6,10 @@ import requests
 
 @bp.route('/')
 def hello_From_controller():
-    return render_template("index.html")
+    API_KEY = 'a6a27a0d825d27f290184340b4de29ce3080eecb'
+    CONTRACT_NAME = 'dublin'
+    stations = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract=' + CONTRACT_NAME + '&apiKey=' + API_KEY ).json()
+    return render_template("index.html",stations = stations)
 
 
 @bp.route('/getData')
@@ -15,5 +18,4 @@ def getAPIData():
     CONTRACT_NAME = 'dublin'
     data = requests.get(
         'https://api.jcdecaux.com/vls/v1/stations?contract=' + CONTRACT_NAME + '&apiKey=' + API_KEY + '')
-    print(data.json())
     return data.json()
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,41 +1,131 @@
 // Initialize and add the map
-let map;
+var map;
+var stationPositions = [];
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: 53.3498, lng: -6.2603 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 14,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+  var position;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        position = {lat: position.coords.latitude, lng: position.coords.longitude}
+        map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 15,
+          center: position,
+          mapTypeControl: false,
+          fullscreenControl:false,
+          streetViewControl: false,
+          zoomControl:false,
+          keyboardShortcuts:false,
+          styles: [
+              { elementType: 'geometry', stylers: [{color: '#f5f5f5'}] },
+              { elementType: 'labels.icon', stylers: [{visibility: 'off'}] },
+              { elementType: 'labels.text.fill', stylers: [{color: '#1b1b1b'}] },
+              { elementType: 'labels.text.stroke', stylers: [{color: '#f5f5f5'}] },
+              {
+                  featureType: 'water',
+                  elementType: 'geometry',
+                  stylers: [{color: '#c9c9c9'}]
+              },
+              {
+                  featureType: 'landscape',
+                  elementType: 'geometry',
+                  stylers: [{color: '#e3e3e3'}]
+              }
+        ]
+      });
+      const marker = new google.maps.Marker({
+          map: map,
+          position: position,
+          title: "Your Location",
+          icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' // Set custom icon
+      });
+      getStationCoordinates()
+    }, function() {
+        alert('Error: The Geolocation service failed.');
+    });
+  } else {
+      alert('Error: Your browser doesn\'t support geolocation.');
+  }
 }
 
-initMap();
+function getStationCoordinates(){
+
+  fetch('/getData')
+  .then(response => response.json())
+      .then(stations => {
+        stations.forEach(function(station) {
+            addMarkerWithLabel({lat: station.position.lat, lng: station.position.lng}, station);
+        });
+        stationPositions = stations;
+        console.log(stationPositions);
+      })
+      .catch(error => {
+          console.error('Error fetching data:', error);
+      });
+}
+
+function bindInfoWindow(marker, map, infowindow, html) {
+    google.maps.event.addListener(marker, 'mouseover', function () {
+        infowindow.setContent(html);
+        infowindow.open(map, marker);
+    });
+}
+function addMarkerWithLabel(position, obj) {
+    var infowindow =  new google.maps.InfoWindow({
+	content: '',
+	map: map
+});
+  const marker = new google.maps.Marker({
+    map: map,
+    position: position,
+    title: obj.name
+  });
+  marker.addListener('mouseover', function() {
+    infowindow.open(map, this);
+    // infowindow.setContent(obj.name);
+      bindInfoWindow(marker, map, infowindow, `
+        <div>
+            <p>
+                <b>Name</b>: ${obj.name}
+            </p>
+            <p>
+                <b>Total Bike Stands</b>: ${obj.bike_stands}
+            </p>
+            <p>
+                <b>Total Available Bike Stands</b>: ${obj.available_bike_stands}
+            </p>
+            <p>
+               <b>Available Bikes</b>: ${obj.available_bikes}
+            </p>
+        </div>
+      `);
+});
+
+// assuming you also want to hide the infowindow when user mouses-out
+marker.addListener('mouseout', function() {
+    infowindow.close();
+});
+}
 
 function search(){
   var geocoder = new google.maps.Geocoder();
   var address = document.getElementById("search-input").value;
 
+  var mapStyles = [
+    {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+            { visibility: "off" }
+        ]
+    }
+  ];
+
   geocoder.geocode({
     'address': address
   }, async function (results, status) {
     var latitude = results[0].geometry.location.lat();
-      var longitude = results[0].geometry.location.lng();
-      const position = { lat: latitude, lng: longitude };
+    var longitude = results[0].geometry.location.lng();
+    const position = { lat: latitude, lng: longitude };
     if (status == google.maps.GeocoderStatus.OK) {
       let Map;
       ({Map} = await google.maps.importLibrary("maps"));
@@ -46,6 +136,7 @@ function search(){
         zoom: 14,
         center: position,
         mapId: "DEMO_MAP_ID",
+        styles : mapStyles
       });
 
       // The marker, positioned at Uluru
@@ -57,4 +148,3 @@ function search(){
     }
   });
 }
-
