f72fd5942ffad69078557a68adaae50c1f216ffe
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2024-04-15 16:19:46 +0100

    1. app.py file added

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from app import create_app
 from config import Config
 app = create_app(Config)
 
-if __name__ == '__main__':
+def main():
     app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    main()
