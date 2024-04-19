8b8907f0026989012b6bb1fe3683dc2b47b68001
Naveen <naveen.jaisankar1999@gmail.com>
2024-04-19 09:52:20 +0100

    timemap and weekday map added

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -647,13 +647,50 @@ $(document).ready(function() {
     $select.trigger('change');
 });
 
+var weekdayMap = {
+    'Monday': 1,
+    'Tuesday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6,
+    'Sunday': 7
+  };
+
+  var timeMap = {
+    '00:00': 1,
+    '01:00': 2,
+    '02:00': 3,
+    '03:00': 4,
+    '04:00': 5,
+    '05:00': 6,
+    '06:00': 7,
+    '07:00': 8,
+    '08:00': 9,
+    '09:00': 10,
+    '10:00': 11,
+    '11:00': 12,
+    '12:00': 13,
+    '13:00': 14,
+    '14:00': 15,
+    '15:00': 16,
+    '16:00': 17,
+    '17:00': 18,
+    '18:00': 19,
+    '19:00': 20,
+    '20:00': 21,
+    '21:00': 22,
+    '22:00': 23,
+    '23:00': 24
+  };
+
 function selectDay(day) {
      document.getElementById('dayDropDown').textContent = day;
-     document.getElementById('selectedDay').value = day;
+     document.getElementById('selectedDay').value =weekdayMap[day] ;
   }
 
   function selectTime(time) {
     document.getElementById('timeDropDown').textContent = time;
-    document.getElementById('selectedTime').value = time;
+    document.getElementById('selectedTime').value =timeMap[time];
     // Close the dropdown if needed
   }
